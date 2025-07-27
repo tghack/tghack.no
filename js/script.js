09927df1a94283ee487592d1de9076ed0afa25c1
Simen Lybekk <simen@lybekk.no>
2025-07-27 19:06:24 +0200

    Add days to countdown

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const countdownTimer = setInterval(function() {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -15,6 +16,13 @@ const countdownTimer = setInterval(function() {
     if (distance < 0) {
         countdownEl.innerText = customMessage;
     } else {
-        countdownEl.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        let text = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+        if (days > 0) {
+            const article = days == 1 ? "" : "er";
+            text = `${days} dag${article}, ${text}`;
+        }
+
+        countdownEl.innerText = text;
     }
 }, 1000);
