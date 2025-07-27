88fe112978fc7e4baddbb569a4923385f9881d01
Simen Lybekk <simen@lybekk.no>
2025-07-27 19:03:30 +0200

    Tweak countdown

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+let countdownEl = document.getElementById("countdown");
+countdownEl.className += " enabled";
 
 const targetDate = new Date('2026-04-01T19:00:00').getTime();
-const customMessage = `CTF'en er LIVE nå @ <a color="white" href="https://ctf.tghack.no/">ctf.tghack.no</a>`;                                                                                                                                           // TG25{7juvst4rter}
+const customMessage = `CTF'en er LIVE nå @ <a color="white" href="https://ctf.tghack.no/">ctf.tghack.no</a>`;
 
-
-const countdown = setInterval(function() {
+const countdownTimer = setInterval(function() {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
@@ -11,10 +12,9 @@ const countdown = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = 
-        `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-
     if (distance < 0) {
-        document.getElementById("countdown").innerHTML = customMessage;
+        countdownEl.innerText = customMessage;
+    } else {
+        countdownEl.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 }, 1000);
