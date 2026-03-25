a8a4d14815995e7e347072cc9c904b59536b00d8
Bendik Dyrli <skandix@datapor.no>
2026-03-26 00:07:44 +0100

    lost in translation
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,13 +5,14 @@
 
     <div class="text-wrap">
         <p class="mb-5">
-            TG:HACK er en nybegynnervennlig, jeopardy-basert CTF. Den arrangeres både fysisk (onsite) og over nett (offsite), og fungerer også som et wargame. 
+            TG:HACK er en nybegynnervennlig, jeopardy-basert CTF. Den arrangeres både fysisk (onsite) og over nett (offsite), og fungerer også som et wargame.
 
             Vårt fokus er å lage veiledende oppgaver for nybegynnere, samtidig som vi tilbyr utfordrende oppgaver for mer erfarne deltakere.
         </p>
 
         <p class="mb-5">
-            Hver påske arrangerer The Gathering en tre dager lang CTF i Vikingskipet i Hamar. Det kreves en gyldig billett til arrangementet for å kunne vinne premier. I tillegg krever noen oppgaver (f.eks. låseplukking og maskinvareoppgaver) at deltakerne er fysisk til stede, så du bør prøve å bli med oss i Vikingskipet.
+            Hver påske arrangerer The Gathering en tre dager lang CTF i Vikingskipet i Hamar. Det kreves en gyldig billett til
+            arrangementet for å kunne vinne premier. I tillegg krever noen oppgaver (f.eks. dirking av låser og maskinvareoppgaver) at deltakerne er fysisk til stede, så du bør prøve å bli med oss i Vikingskipet.
         </p>
     </div>
 </div>
