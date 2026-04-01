d904e7f17ab0f422ee6667cd9c27c60dd0965ada
Bendik Dyrli <bendik.dyrli@gmail.com>
2026-04-01 17:06:20 +0200

    PLAY NOW .
    Signed-off-by: Bendik Dyrli <bendik.dyrli@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import bg2 from '$lib/assets/background/02.png';
 
-	import Countdown from '$lib/components/Countdown.svelte';
 	import logo from "$lib/assets/logo/hvit_horisontal.svg";
+	import Countdown from '$lib/components/Countdown.svelte';
 
 </script>
 
@@ -19,13 +19,20 @@
 
     <div class="relative flex items-center justify-center h-full">
 		<div class="text-center">
-            <div class="flex items-center justify-center py-18">
-                <img class="scale-85 md:scale-200" src={logo} fetchpriority="high" alt="tghack logo"/>
-            </div>
+				<div class="flex items-center justify-center py-18">
+					<img class="scale-85 md:scale-200" src={logo} fetchpriority="high" alt="tghack logo"/>
+				</div>
 			<Countdown />
-			<div class="mt-8 text-lg sm:text-xl text-white px-8">
+			<div class="mt-8 text-lg sm:text-xl text-white px-8 py-8">
 				Norges lengstlevende CTF, som del av Norges største datafestival og LAN-party: <a href="https://www.tg.no/" class="underline hover:text-slate-400">The Gathering</a>
 			</div>
+				<a href="https://ctf.tghack.no" class="px-8 py-4 text-xl font-bold text-white 
+					bg-black border-2 border-green-400 rounded-xl
+					shadow-[0_0_20px_rgba(74,222,128,0.8)] 
+					hover:shadow-[0_0_30px_rgba(74,222,128,1)] 
+					transition duration-300">
+					Spill!
+				</a>
 		</div>
 	</div>
 
